344748c52a0f5bfba9d9c121b2d9a71a86c58389
Dawid Medrek <dawmd55@gmail.com>
2023-07-07 12:05:49 +0200

    Demos using arbitrary parameters

--- demos/quic_echo_server.cc
+++ demos/quic_echo_server.cc
@@ -22,11 +22,16 @@
 // Demonstration of ability to accept QUIC connection and echo-ing the received data.
 
 #include <seastar/core/app-template.hh>
-#include <seastar/core/reactor.hh>
 #include <seastar/core/future-util.hh>
-#include <seastar/net/api.hh>
-#include <seastar/net/quic.hh>
+#include <seastar/core/reactor.hh>
 #include <seastar/core/sleep.hh>
+#include <seastar/net/api.hh>
+#include <seastar/net/inet_address.hh>
+#include <seastar/net/quic.hh>
+
+#include <string_view>
+
+namespace bpo = boost::program_options;
 
 constexpr static std::uint64_t STREAM_ID = 4;
 
@@ -35,14 +40,13 @@ seastar::future<> handle_connection(seastar::net::quic_accept_result accept_resu
     auto conn = std::move(accept_result.connection);
     auto in = conn.input(STREAM_ID);
     auto out = conn.output(STREAM_ID);
-    return seastar::do_with(std::move(conn), std::move(in), std::move(out), [](auto &conn, auto &in, auto &out) {
-        return seastar::keep_doing([&in, &out]() {
-            return in.read().then([&out](seastar::temporary_buffer<char> buf) {
-                char msg_buf[buf.size() + 1];
-                memcpy(msg_buf, buf.get(), buf.size());
-                msg_buf[buf.size()] = '\0';
-                std::cout << "Received message: " << msg_buf << std::endl;
-                return out.write(std::move(buf)).then([&out]() {
+
+    return seastar::do_with(std::move(conn), std::move(in), std::move(out), [] (auto& conn, auto& in, auto& out) {
+        return seastar::keep_doing([&in, &out] {
+            return in.read().then([&out] (seastar::temporary_buffer<char> buf) {
+                const auto msg = std::string_view{ buf.begin(), buf.size() };
+                std::cout << "Received message: " << msg << std::endl;
+                return out.write(std::move(buf)).then([&out] {
                     return out.flush();
                 });
             });
@@ -50,22 +54,34 @@ seastar::future<> handle_connection(seastar::net::quic_accept_result accept_resu
     });
 }
 
-seastar::future<> service_loop() {
-    // TODO: Either add keys to the repo or generate them here.
-    std::string cert_file = "/home/danielmastalerz/Pulpit/seastar_quic/quiche/quiche/examples/cert.crt";
-    std::string key_file = "/home/danielmastalerz/Pulpit/seastar_quic/quiche/quiche/examples/cert.key";
-
-    return seastar::do_with(seastar::net::quic_listen(seastar::make_ipv4_address({1234}), cert_file, key_file),
-                            [](auto &listener) {
-                                return seastar::keep_doing([&listener]() {
-                                    return listener.accept().then([](seastar::net::quic_accept_result result) {
-                                        (void) handle_connection(std::move(result));
-                                    });
-                                });
-                            });
+seastar::future<> service_loop(const uint16_t port, const std::string address,
+        const std::string cert_file, const std::string key_file)
+{
+    return seastar::do_with(seastar::net::quic_listen({ seastar::net::inet_address(address), port }, cert_file, key_file),
+        [] (auto& listener) {
+                return seastar::keep_doing([&listener] {
+                    return listener.accept().then([] (auto result) {
+                        (void) handle_connection(std::move(result));
+                    });
+                });
+            }
+    );
 }
 
-int main(int ac, char **av) {
+int main(int ac, char** av) {
     seastar::app_template app;
-    return app.run(ac, av, service_loop);
+    app.add_options()
+            ("port", bpo::value<uint16_t>()->default_value(1234), "Server port")
+            ("address", bpo::value<std::string>()->default_value("127.0.0.1"), "Server IP address")
+            ("cert,c", bpo::value<std::string>()->required(), "Server certificate file")
+            ("key,k", bpo::value<std::string>()->required(), "Certificate key");
+
+    return app.run(ac, av, [&] {
+        auto&& config = app.configuration();
+        auto port = config["port"].as<uint16_t>();
+        auto address = config["address"].as<std::string>();
+        auto crt = config["cert"].as<std::string>();
+        auto key = config["key"].as<std::string>();
+        return service_loop(port, address, crt, key);
+    });
 }
--- demos/quic_receive_big_chunk_of_data.cc
+++ demos/quic_receive_big_chunk_of_data.cc
@@ -22,25 +22,27 @@
 // Demonstration of ability to receive big chunks of data.
 
 #include <seastar/core/app-template.hh>
-#include <seastar/core/reactor.hh>
 #include <seastar/core/future-util.hh>
-#include <seastar/net/api.hh>
-#include <seastar/net/quic.hh>
+#include <seastar/core/reactor.hh>
 #include <seastar/core/sleep.hh>
+#include <seastar/net/api.hh>
+#include <seastar/net/inet_address.hh>
+#include <seastar/net/quic.hh>
 
-constexpr static std::uint64_t STREAM_ID = 4;
+namespace bpo = boost::program_options;
 
-static size_t rcv_bytes = 0;
-static size_t accepted_conns = 0;
-
-std::vector<seastar::future<>> vec;
+constexpr static uint64_t STREAM_ID = 4;
+constexpr static size_t MAX_ACCEPTED_CONNECTIONS = 3;
 
 seastar::future<> handle_connection(seastar::net::quic_accept_result accept_result) {
+    static size_t rcv_bytes = 0;
+
     std::cout << "Accepted connection!" << std::endl;
     auto conn = std::move(accept_result.connection);
     auto in = conn.input(STREAM_ID);
-    return seastar::do_with(std::move(conn), std::move(in), [](auto &conn, auto &in) {
-         return seastar::do_until([&in] () { return in.eof(); }, [&in]() {
+
+    return seastar::do_with(std::move(conn), std::move(in), [] (auto& conn, auto& in) {
+         return seastar::do_until([&in] { return in.eof(); }, [&in] {
             return in.read().then([](seastar::temporary_buffer<char> buf) {
                 if (buf.empty()) {
                     return seastar::make_ready_future<>();
@@ -50,34 +52,49 @@ seastar::future<> handle_connection(seastar::net::quic_accept_result accept_resu
                 std::cout << "Received " << buf.size() << " bytes, total " << rcv_bytes << "b" << std::endl;
                 return seastar::make_ready_future();
             });
-        }).then([] () {
-           std::cout << "Connection processed!\n";
+        }).then([] {
+           std::cout << "Connection processed!" << std::endl;
         });
     });
 }
 
-seastar::future<> service_loop() {
-    // TODO: Either add keys to the repo or generate them here.
-    std::string cert_file = "/home/muzarski/sem5/zpp/seastar_quic/quiche/quiche/examples/cert.crt";
-    std::string key_file = "/home/muzarski/sem5/zpp/seastar_quic/quiche/quiche/examples/cert.key";
+seastar::future<> service_loop(const uint16_t port, const std::string address,
+        const std::string cert_file, const std::string key_file)
+{
+    static size_t accepted_connections = 0;
+    static std::vector<seastar::future<>> vec;
 
-    return seastar::do_with(seastar::net::quic_listen(seastar::make_ipv4_address({1234}), cert_file, key_file),
-                            [](auto &listener) {
-                                return seastar::do_until([] () { return accepted_conns >= 3; }, [&listener]() {
-                                    return listener.accept().then([](seastar::net::quic_accept_result result) {
-                                        accepted_conns++;
-                                        vec.push_back(handle_connection(std::move(result)));
-                                    });
-                                }).then([&listener] () {
-                                   listener.abort_accept();
-                                   return seastar::when_all(vec.begin(), vec.end()).then([] (auto res) {
-                                      return seastar::make_ready_future<>();
-                                   });
-                                });
-                            });
+    return seastar::do_with(seastar::net::quic_listen({ seastar::net::inet_address(address), port }, cert_file, key_file),
+        [] (auto& listener) {
+            return seastar::do_until([] { return accepted_connections >= MAX_ACCEPTED_CONNECTIONS; }, [&listener] {
+                return listener.accept().then([] (auto result) {
+                    ++accepted_connections;
+                    vec.push_back(handle_connection(std::move(result)));
+                });
+            }).then([&listener] {
+                listener.abort_accept();
+                return seastar::when_all(vec.begin(), vec.end()).then([] (auto result) {
+                    return seastar::make_ready_future<>();
+                });
+            });
+        }
+    );
 }
 
-int main(int ac, char **av) {
+int main(int ac, char** av) {
     seastar::app_template app;
-    return app.run(ac, av, service_loop);
+    app.add_options()
+            ("port", bpo::value<uint16_t>()->default_value(1234), "Server port")
+            ("address", bpo::value<std::string>()->default_value("127.0.0.1"), "Server IP address")
+            ("cert,c", bpo::value<std::string>()->required(), "Server certificate file")
+            ("key,k", bpo::value<std::string>()->required(), "Certificate key");
+
+    return app.run(ac, av, [&] {
+        auto&& config = app.configuration();
+        auto port = config["port"].as<uint16_t>();
+        auto address = config["address"].as<std::string>();
+        auto crt = config["cert"].as<std::string>();
+        auto key = config["key"].as<std::string>();
+        return service_loop(port, address, crt, key);
+    });
 }
